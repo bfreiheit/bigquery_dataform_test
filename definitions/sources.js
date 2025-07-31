689ce37744e35bcaf32e9921b328b7e1568aa26f
belinda freiheit <belinda.freiheit@gmail.com>
2025-07-31 19:32:33 +0000

    add files

---
+++ definitions/sources.js
@@ -0,0 +1,21 @@
+
+declare({
+  schema: "analytics_stage",
+  name: "sessions",
+  description: "Raw session events from the event tracking system"
+});
+
+declare({
+  schema: "analytics_stage",
+  name: "leases"
+});
+
+declare({
+  schema: "analytics_stage",
+  name: "channel_mapping"
+});
+
+declare({
+  schema: "analytics_stage",
+  name: "ads_benchmarks"
+});
